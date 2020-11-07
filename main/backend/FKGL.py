aa9d9e20bfdebb6d13544067906f24ca9bda073b
dhararya <aryan.dhar@hotmail.com>
2020-11-07 17:43:41 -0500

    -test

---
+++ main/backend/FKGL.py
@@ -0,0 +1,26 @@
+import nltk
+import syllables
+class audience_appropriateness(FACTOR):
+    def score(text, audience):
+        #defining appropriate FKGL score for each audience level
+        desired_scores = {"basic": 70.0,  "intermediate": 55.0, "difficult" : 40.0}
+        """the FKGL score is computed using the following equation: 
+        206.835-1.015(total words/total sentences)-84.6(total syllables/total words)"""
+        total_sentences = len(nltk.sent_tokenize(text))
+        words = nltk.word_tokenize(text)
+        total_words = len(words)
+        total_syllables = 0
+        for word in words:
+            total_syllables += syllables.estimate(word)
+        fkgl_score = 206.835-1.015*(total_words/total_sentences)-84.6*(total_syllables/total_words)
+        #Calculating how many multiples of 5 it is away from the desired fkgl
+        deviation = (abs(fkgl_score - desired_scores[audience]))/5
+        to_subtract = deviation*0.2
+        if to_subtract > 1:
+            grade = 0
+        else:
+            grade = 1-to_subtract
+        return grade
+
+
+
