f3e49d492503e83ee1983d902764d2284595a8de
Urvi <urvi.verk@gmail.com>
2020-11-08 11:26:00 +0800

    edited random.css

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Leaderboard (models.Model):
+    '''Creates a table with username, room, and score'''
+    username = models.CharField(max_length=50)
+    room = models.CharField(max_length=50)
+    score = models.IntegerField()
